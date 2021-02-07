313d5d64dabfbc6d078e1dcf4c07e8a15f77007c
TeknoCepni <andopme@gmail.com>
2021-02-07 21:02:57 +0300

    added createWorker method

--- app/src/main/java/work/manager/workmanagerexample/MainActivity.kt
+++ app/src/main/java/work/manager/workmanagerexample/MainActivity.kt
@@ -2,7 +2,11 @@ package work.manager.workmanagerexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +18,22 @@ class MainActivity : AppCompatActivity() {
         switch1.setOnCheckedChangeListener { _, isChecked ->
             if (!isChecked) {
                 sharedPreference.changeSwitch(false)
+                createWorker()
             } else {
                 sharedPreference.changeSwitch(true)
             }
         }
+    }
+
+    private fun createWorker() {
+        val workRequest: WorkRequest =
+                OneTimeWorkRequestBuilder<SwitchControlWorker>()
+                        .setInitialDelay(5, TimeUnit.MINUTES)
+                        .build()
+
+        WorkManager
+                .getInstance(this)
+                .enqueue(workRequest)
+
     }
 }
